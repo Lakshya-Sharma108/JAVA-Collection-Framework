0f1ccc92d44e6be9dd9a3efa1807327071c58180
Lakshaya Awasthi <lakshayaawasthi108@gmail.com>
2026-04-23 13:46:19 +0530

    add ListIntro class with overview of Java List interface and its implementations

---
+++ A_ListInterface/ListIntro.java
@@ -0,0 +1,28 @@
+package A_ListInterface;
+
+public class ListIntro {
+    public static void main(String[] args) {
+        /*
+            Java List Interface:
+            In java, the List interface is an ordered collection that allows us to store and access elements sequentially.
+
+            We can not create object of List because it's an Interface
+            Example:
+            List l1 = new List();   <- Wrong
+
+            Instead:
+            List l1 = new ArrayList();   <- Right
+            List l1 = new LinkedList();   <- Right
+
+            We can provide the SubClass or Concrete class reference to List variable - (upcasting)
+
+
+            SubClass or Concrete class of List:
+            1. ArrayList
+            2. LinkedList
+            3. Stack
+            4. Vector
+
+        */
+    }
+}
