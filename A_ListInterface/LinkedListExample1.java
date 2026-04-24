3c5987b52503b2cb7bd261a9fef2fa62bf042664
Lakshaya Awasthi <lakshayaawasthi108@gmail.com>
2026-04-24 19:59:32 +0530

    add LinkedListExample1 with explanation of LinkedList structure and hierarchy

---
+++ A_ListInterface/LinkedListExample1.java
@@ -0,0 +1,36 @@
+package A_ListInterface;
+
+public class LinkedListExample1 {
+    public static void main(String[] args) {
+        /*
+            What is LinkedList?
+            The LinkedList class of java collection framework provides the functionality of the linked list data structure (doubly linkedlist)
+            -> Each element in a LinkedList is known as a node, it consists of 3 fields
+
+
+            LinkedList is just a collection of doubly LinkedList nodes:
+            How a node looks like?
+                            +-------+-------+-------+
+               Node ->      | Prev  | Data  | Next  |
+                            +-------+-------+-------+
+
+            -> Prev: stores an address of the previous element in the list, Null for first element
+            -> Next: stores an address of the next element in the list, Null for the last element
+            -> Data: stores the actual data
+
+
+
+
+
+
+            LinkedList Collection HIERARCHY ==>
+                                                   Iterable   -> interface
+                                                      |
+                                                  Collection   -> interface
+                                                      |
+                                                     List      -> interface
+                                                      |
+                                                   LinkedList   -> class
+        */
+    }
+}
