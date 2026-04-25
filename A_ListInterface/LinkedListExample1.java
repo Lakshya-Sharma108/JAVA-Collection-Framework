aee576e73e73a3aedf8afa5a533ffbf41af700e7
Lakshaya Awasthi <lakshayaawasthi108@gmail.com>
2026-04-25 08:29:52 +0530

    add LinkedListExample1 with explanations on memory allocation and creation methods

--- A_ListInterface/LinkedListExample1.java
+++ A_ListInterface/LinkedListExample1.java
@@ -1,5 +1,10 @@
 package A_ListInterface;
 
+import java.util.Collection;
+import java.util.Collections;
+import java.util.LinkedList;
+import java.util.List;
+
 public class LinkedListExample1 {
     public static void main(String[] args) {
         /*
@@ -19,6 +24,8 @@ public class LinkedListExample1 {
             -> Data: stores the actual data
 
 
+            LinkedList allocated on none contiguous memory locations, so it is not necessary to have a continuous block of memory to store a LinkedList. It can be stored in different memory locations and each node will have the address of the next node.
+
 
 
 
@@ -31,6 +38,24 @@ public class LinkedListExample1 {
                                                      List      -> interface
                                                       |
                                                    LinkedList   -> class
+
         */
+
+
+        // Creation of LinkedList -> without bounding in any datatype
+        // Collection linkedList = new LinkedList();  // first way
+        // List linkedListt = new LinkedList();  // second way
+        // LinkedList linkedListtt = new LinkedList();  // third way
+
+
+        // Creation of LinkedList -> with bounding in a datatype
+        List<Integer> linkedList1 = new LinkedList<>();
+
+        // adding elements
+        linkedList1.add(10);
+        linkedList1.add(20);
+        linkedList1.add(30);
+        System.out.println(linkedList1);
+
     }
 }
