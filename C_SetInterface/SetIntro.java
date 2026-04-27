63a5556b5facde025991573237396ce096a9b892
Lakshaya Awasthi <lakshayaawasthi108@gmail.com>
2026-04-27 10:54:49 +0530

    add SetIntro class with overview of Java Set interface and hierarchy

---
+++ C_SetInterface/SetIntro.java
@@ -0,0 +1,28 @@
+package C_SetInterface;
+
+public class SetIntro {
+    public static void main(String[] args) {
+        /*
+            Java Set Interface:
+            The set interface of the java collection framework provides the feature of the mathematical set in java. It extends the collection interface, sets cannot contain duplicate elements
+
+
+            Set Hierarchy:
+                                         Collection
+                                             |
+                                            Set
+                                             |
+                                        ArrayDequeue
+                                             |
+                 +------------------+------------------+------------------+
+                 |                  |                  |                  |
+              EnumSet             HashSet         LinkedHashSet         TreeSet
+
+
+            -> Set only stores unique elements, duplicate elements are not allowed
+
+
+
+        */
+    }
+}
