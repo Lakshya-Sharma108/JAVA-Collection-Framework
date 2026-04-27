1fc301439589a3d995a8a216be4c4ddeba70fc16
Lakshaya Awasthi <lakshayaawasthi108@gmail.com>
2026-04-27 11:08:30 +0530

    enhance SetIntro with additional details on set characteristics and implementations

--- C_SetInterface/SetIntro.java
+++ C_SetInterface/SetIntro.java
@@ -6,6 +6,10 @@ public class SetIntro {
             Java Set Interface:
             The set interface of the java collection framework provides the feature of the mathematical set in java. It extends the collection interface, sets cannot contain duplicate elements
 
+            It represents the unordered set of elements which doesn't allow us to store the duplicate items.
+            We can store at most one null value in Set.
+            Set is implemented by HashSet, LinkedHashSet and TreeSet.
+
 
             Set Hierarchy:
                                          Collection
