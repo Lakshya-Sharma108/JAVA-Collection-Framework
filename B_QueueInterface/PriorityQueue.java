0c60d73a390399a604cbb54ed5c2be0ddf8d7b8b
Lakshaya Awasthi <lakshayaawasthi108@gmail.com>
2026-04-27 10:37:11 +0530

    enhance PriorityQueue with detailed explanations and examples of priority handling

--- B_QueueInterface/PriorityQueue.java
+++ B_QueueInterface/PriorityQueue.java
@@ -1,13 +1,23 @@
 package B_QueueInterface;
 
+import java.util.Collections;
 import java.util.Queue;
 
 public class PriorityQueue {
     public static void main(String[] args) {
         /*
             Java Priority Queue:
+            Instead of "first in, first out" (like a normal queue), it always gives you the highest priority element first — regardless of insertion order.
+
+            Imagine you're a doctor in an emergency room. Patients keep arriving — some with minor issues, some critical. You don't treat them in the order they arrived. You treat the most critical first.
+
+            In the case of Integer:
+            "Highest priority" = smallest value (min comes out first)
 
 
+
+            We can reverse the priority using:
+            Comparator.reverseOrder() -> reverse the priority to max
 
         */
 
@@ -18,5 +28,27 @@ public class PriorityQueue {
         pq.offer(30);
         pq.offer(10);
         pq.offer(20);
+
+        // smallest value comes out first
+        System.out.println(pq.poll());   // 10
+        System.out.println(pq.poll());   // 20
+        System.out.println(pq.poll());   // 30
+        System.out.println(pq.poll());   // 40
+
+
+
+
+        // Reversing the priority -> max gets priority
+        Queue<Integer> pq2 = new java.util.PriorityQueue<>(Collections.reverseOrder());
+        pq2.offer(40);
+        pq2.offer(30);
+        pq2.offer(10);
+        pq2.offer(20);
+        System.out.println(pq2.poll());   // 40
+        System.out.println(pq2.poll());   // 30
+        System.out.println(pq2.poll());   // 20
+        System.out.println(pq2.poll());   // 10
+
+
     }
 }
