e266a7b023b0d5c92fec3fbaa38460a612e5f3c2
Lakshaya Awasthi <lakshayaawasthi108@gmail.com>
2026-04-27 10:01:16 +0530

    add PriorityQueue with examples of queue creation and element insertion

---
+++ B_QueueInterface/PriorityQueue.java
@@ -0,0 +1,22 @@
+package B_QueueInterface;
+
+import java.util.Queue;
+
+public class PriorityQueue {
+    public static void main(String[] args) {
+        /*
+            Java Priority Queue:
+
+
+
+        */
+
+
+        // Creation
+        Queue<Integer> pq = new java.util.PriorityQueue<>();
+        pq.offer(40);
+        pq.offer(30);
+        pq.offer(10);
+        pq.offer(20);
+    }
+}
