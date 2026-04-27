e4d14dbab319a048e7cdcd75d50444807f31bab4
Lakshaya Awasthi <lakshayaawasthi108@gmail.com>
2026-04-27 09:22:25 +0530

    add QueueIntro with an overview of the Java Queue interface and its methods

---
+++ B_QueueInterface/QueueIntro.java
@@ -0,0 +1,49 @@
+package B_QueueInterface;
+
+public class QueueIntro {
+    public static void main(String[] args) {
+        /*
+            Java Queue Interface:
+            The queue interface of the java collection framework provides the functionality of the queue data structure. It extends the collection interface
+
+            Think of queue is like "A line on a ticket counter" where first one gets ticket first and new person needs to add in the end
+
+            Queue follows FIFO (First In, First Out)
+
+
+            Queue Hierarchy:
+                                     Collection
+                                         |
+                                       Queue
+                                         |
+                       +------------------------------------+
+                       |                 |                  |
+                    ArrayDequeue     LinkedList        Priority Queue
+
+
+            LinkedList class implements both interfaces (List and Queue)
+
+            Queue:
+                        +-------+  +-------+  +-------+  +-------+
+            Deletion -> |       |  |       |  |       |  |       | <- Addition
+                        +-------+  +-------+  +-------+  +-------+
+                        Front                                 Rear
+
+
+
+            Some Queue specific methods:
+            add() -> inserts the specified elements into the queue, if the task is successful, add() returns true, if not it throws an exception
+
+            offer() -> insert the specified element into the queue, if the task is successful, offer() returns true, if not it returns false
+
+            element() -> Returns the head of the queue, throws an exception if queue is empty
+
+            peek() -> returns the head of the queue, returns null if the queue is empty
+
+            remove() -> Returns and removes the head of the queue, Throws an exception if the queue is empty
+
+            poll() -> returns and removes the head of the queue, returns null if queue is empty
+
+        */
+    }
+}
