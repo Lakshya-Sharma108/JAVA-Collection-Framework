879eda6b75e06280804018d1870ba2c03ab5baad
Lakshaya Awasthi <lakshayaawasthi108@gmail.com>
2026-04-27 09:44:02 +0530

    add ArrayDequeue with an overview of the Java ArrayDeque and its functionality

---
+++ B_QueueInterface/ArrayDequeue.java
@@ -0,0 +1,30 @@
+package B_QueueInterface;
+
+public class ArrayDequeue {
+    public static void main(String[] args) {
+        /*
+            Java ArrayDequeue:
+            Java array dequeue stands for "Doubly Ended Queue" which means in array dequeue, Insertion and Deletion takes places on both end
+
+            You can add or delete elements in dequeue from both side (front side and end side)
+
+
+            DeQueue Hierarchy:
+                                     Collection
+                                         |
+                                       Queue
+                                         |
+                                    ArrayDequeue
+
+
+            DeQueue:
+                            +-------+  +-------+  +-------+  +-------+
+                Deletion -> |       |  |       |  |       |  |       | <- Addition
+                Addition -> |       |  |       |  |       |  |       | <- Deletion
+                            +-------+  +-------+  +-------+  +-------+
+                            Front                                 Rear
+
+
+        */
+    }
+}
