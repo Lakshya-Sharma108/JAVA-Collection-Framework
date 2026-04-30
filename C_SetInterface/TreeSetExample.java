98f1af15ff0144580fe8afff244c2baeb17acc89
Lakshaya Awasthi <lakshayaawasthi108@gmail.com>
2026-04-30 14:20:14 +0530

    add TreeSetExample class with demonstration of TreeSet behavior and properties

---
+++ C_SetInterface/TreeSetExample.java
@@ -0,0 +1,41 @@
+package C_SetInterface;
+
+import java.util.TreeSet;
+
+public class TreeSetExample {
+    public static void main(String[] args) {
+        /*
+            TreeSet:
+
+
+
+        */
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//        TreeSet<String> treeSet1 = new TreeSet<>();
+//        treeSet1.add("Lakshaya");
+//        treeSet1.add("Awasthi");
+//        treeSet1.add("Awasthi");   // duplicate elements, will not be added
+//        treeSet1.add("Yogesh");
+//        treeSet1.add("Deepu");
+//        treeSet1.add("Deepu");   // duplicate elements, will not be added
+//        treeSet1.add("Bheeshm");
+//
+//        System.out.println(treeSet1);
+
+    }
+}
