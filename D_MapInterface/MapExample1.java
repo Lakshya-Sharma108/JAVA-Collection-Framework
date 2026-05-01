ce94eb20578f72c9be1a23ae26810626a9f91a2b
Lakshaya Awasthi <lakshayaawasthi108@gmail.com>
2026-05-01 13:08:42 +0530

    add MapExample1 class with explanation of Java Map interface and its characteristics

---
+++ D_MapInterface/MapExample1.java
@@ -0,0 +1,27 @@
+package D_MapInterface;
+
+public class MapExample1 {
+    public static void main(String[] args) {
+        /*
+            Java Map Interface:
+            In java, elements of map are stored in key/value pairs. Keys are unique values associated with the individual Values. A map cannot contain duplicate keys. And each key is associated with a single value.
+
+
+            Map Hierarchy:
+                                                     Map
+                                                      |
+            +--------------------+--------------------+--------------------+--------------------+
+            |                    |                    |                    |                    |
+            HashMap            TreeMap             EnumMap           LinkedHashMap         WeakHashMap
+
+
+
+            Map Characteristics:
+            No duplicate keys: each key can map to at most one value. However, diff. keys can map to the same value.
+            Key-Value association: It maintains an association of keys to value
+            Implementations: Some of the well known classes that implement the map interface are HashMap, TreeMap, LinkedHasMap, and Hashtable.
+            Order:
+
+        */
+    }
+}
