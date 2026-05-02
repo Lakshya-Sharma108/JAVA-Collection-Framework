b7edbac19140cfb10a120dd8b218f0c2036a36e1
Lakshaya Awasthi <lakshayaawasthi108@gmail.com>
2026-05-02 10:20:26 +0530

    remove unnecessary comments in Main class regarding Comparator interface

--- F_ComparatorInterface/Main.java
+++ F_ComparatorInterface/Main.java
@@ -9,16 +9,6 @@ public class Main {
     public static void main(String[] args) {
         /*
             Comparator Interface:
-            Purpose: Defines a natural ordering for the objects of the classes that implement it.
-
-            Method to implement: compareTo(Object obj)
-                Functionality: This method compares the current object with the specified object to determine their order
-
-                Return Value: Returns a negative integer, zero or positive integer as this object is less than, equal to or greater than the specified object, respectively
-
-            Usage Context: Useful when there is single, natural ordering of the objects (e.g. alphabetical order for string, numerical order for numbers)
-
-            Integration: Automatically used by sorting methods in collections that do not specify a custom comparator (e.g. Collections.sort(list) when sorting a list of objects that implement a comparable)
 
         */
 
