568587d4a1978843c1c63fe247db3ed3fefc0dd8
Lakshaya Awasthi <lakshayaawasthi108@gmail.com>
2026-05-02 10:17:03 +0530

    add Main and Student classes with WeightComparator for sorting Student objects

---
+++ F_ComparatorInterface/Main.java
@@ -0,0 +1,65 @@
+package F_ComparatorInterface;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+        /*
+            Comparator Interface:
+            Purpose: Defines a natural ordering for the objects of the classes that implement it.
+
+            Method to implement: compareTo(Object obj)
+                Functionality: This method compares the current object with the specified object to determine their order
+
+                Return Value: Returns a negative integer, zero or positive integer as this object is less than, equal to or greater than the specified object, respectively
+
+            Usage Context: Useful when there is single, natural ordering of the objects (e.g. alphabetical order for string, numerical order for numbers)
+
+            Integration: Automatically used by sorting methods in collections that do not specify a custom comparator (e.g. Collections.sort(list) when sorting a list of objects that implement a comparable)
+
+        */
+
+
+
+
+        // Creating a arraylist
+        List<Integer> list = new ArrayList<>();
+        list.add(15);
+        list.add(8);
+        list.add(2);
+        list.add(80);
+
+        System.out.println(list);   // getting data as insertion order
+
+        Collections.sort(list);  // sorting the list
+
+        System.out.println(list);
+
+
+
+
+
+
+        // Now how to sort a list of custom objects, because an object contains many properties, so basis of which property we will sort the list of objects
+
+        List<Student> students = new ArrayList<>();
+        students.add(new Student("lakshaya", 22, 60));
+        students.add(new Student("divya", 20, 40));
+        students.add(new Student("ankit", 23, 65));
+        students.add(new Student("shruti", 20, 46));
+
+        System.out.println(students);
+
+       Collections.sort(students, new WeightComparator());
+
+        System.out.println(students);
+
+
+
+
+
+    }
+}
---
+++ F_ComparatorInterface/Student.java
@@ -0,0 +1,47 @@
+package F_ComparatorInterface;
+
+public class Student {
+    public String name;
+    public int age;
+    public int weight;
+
+    public Student(String name, int age, int weight) {
+        this.name = name;
+        this.age = age;
+        this.weight = weight;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public int getWeight() {
+        return weight;
+    }
+
+    public void setWeight(int weight) {
+        this.weight = weight;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                ", weight=" + weight +
+                '}';
+    }
+
+}
